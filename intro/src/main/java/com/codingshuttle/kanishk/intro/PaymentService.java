319a882eea3385cba125a4368ae5e1ae9ed592cf
AL84861 <KumarKanishk.Pandey@carelon.com>
2026-05-29 20:09:36 +0530

    beans

---
+++ intro/src/main/java/com/codingshuttle/kanishk/intro/PaymentService.java
@@ -0,0 +1,10 @@
+package com.codingshuttle.kanishk.intro;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class PaymentService {
+public void pay(){
+    System.out.println("paying");
+}
+}
