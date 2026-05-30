dc1db771316704d7d85df5fab11cc3bf3b9f5478
AL84861 <KumarKanishk.Pandey@carelon.com>
2026-05-30 15:47:18 +0530

    module 1

---
+++ intro/src/main/java/com/codingshuttle/anuj/module1Introduction/NotificationService.java
@@ -0,0 +1,7 @@
+package com.codingshuttle.anuj.module1Introduction;
+
+public interface NotificationService {
+
+    void send(String message);
+
+}
---
+++ intro/src/main/java/com/codingshuttle/anuj/module1Introduction/impl/EmailNotificationService.java
@@ -0,0 +1,20 @@
+package com.codingshuttle.anuj.module1Introduction.impl;
+
+import com.codingshuttle.anuj.module1Introduction.NotificationService;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperties;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+//@Primary
+@Component
+@Qualifier("emailNotif")
+//@ConditionalOnProperty(name = "notification.type", havingValue = "email")
+public class EmailNotificationService implements NotificationService {
+
+    @Override
+    public void send(String message) {
+        System.out.println("Email sending... "+message);
+    }
+}
